98e0031eb0d48560928c0572e5bc70eaf8e33cc6
NLRX-WJC <414961362@qq.com>
2020-04-26 13:31:19 +0800

    初始化项目

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button } from 'antd';
+function App() {
+  return (
+    <div className="App">
+       <Button type="primary">Button</Button>
+    </div>
+  );
+}
+
+export default App;
