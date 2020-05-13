765f898ff61f6a33a3048a127b2ca3bed8359250
NLRX-WJC <414961362@qq.com>
2020-05-13 17:48:04 +0800

    build: 将环境变量抽离出来

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,40 +1,35 @@
-import axios from 'axios'
-import store from '@/store'
-import { Modal } from 'antd';
-import { getToken } from '@/utils/auth'
+import axios from "axios";
+import store from "@/store";
+import { Modal } from "antd";
+import { getToken } from "@/utils/auth";
 import { logout } from "@/store/actions";
 
 //创建一个axios示例
 const service = axios.create({
-  baseURL: '', // api 的 base_url
-  // timeout: 5000, // request timeout
-  // headers: {
-  //   'Content-Type': 'application/x-www-form-urlencoded'
-  // }
-})
-
+  baseURL: process.env.REACT_APP_BASE_API, // api 的 base_url
+  timeout: 5000, // request timeout
+});
 
 // 请求拦截器
 service.interceptors.request.use(
-  config => {
+  (config) => {
     // Do something before request is sent
     if (store.getState().user.token) {
       // 让每个请求携带token-- ['Authorization']为自定义key 请根据实际情况自行修改
-      config.headers.Authorization = getToken()
+      config.headers.Authorization = getToken();
     }
-    return config
+    return config;
   },
-  error => {
+  (error) => {
     // Do something with request error
-    console.log(error) // for debug
-    Promise.reject(error)
+    console.log(error); // for debug
+    Promise.reject(error);
   }
-)
-
+);
 
 // 响应拦截器
 service.interceptors.response.use(
-  response => response,
+  (response) => response,
   /**
    * 下面的注释为通过在response里，自定义code来标示请求状态
    * 当code返回如下情况则说明权限有问题，登出并返回到登录页
@@ -68,26 +63,27 @@ service.interceptors.response.use(
   //     return response.data
   //   }
   // },
-  error => {
-    console.log('err' + error) // for debug
-    const { status } = error.response
+  (error) => {
+    console.log("err" + error); // for debug
+    const { status } = error.response;
     if (status === 403) {
       Modal.confirm({
-        title: '确定登出?',
-        content: '由于长时间未操作，您已被登出，可以取消继续留在该页面，或者重新登录',
-        okText: '重新登录',
-        cancelText: '取消',
+        title: "确定登出?",
+        content:
+          "由于长时间未操作，您已被登出，可以取消继续留在该页面，或者重新登录",
+        okText: "重新登录",
+        cancelText: "取消",
         onOk() {
-          let token = store.getState().user.token
-          store.dispatch(logout(token))
+          let token = store.getState().user.token;
+          store.dispatch(logout(token));
         },
         onCancel() {
-          console.log('Cancel');
+          console.log("Cancel");
         },
       });
     }
-    return Promise.reject(error)
+    return Promise.reject(error);
   }
-)
+);
 
-export default service
+export default service;
