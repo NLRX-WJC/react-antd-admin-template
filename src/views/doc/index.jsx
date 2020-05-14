92fd6bdb1d5d00a9c5d0f2013d0bd304392907b6
NLRX-WJC <414961362@qq.com>
2020-05-14 11:17:13 +0800

    feat: 添加开发文档

--- src/views/doc/index.jsx
+++ src/views/doc/index.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import TypingCard from '@/components/TypingCard'
 const Doc = () => {
-  const cardContent = `开发文档正在编写中...`
+  const cardContent = `
+    开发文档请戳这里 <a href="https://nlrx-wjc.github.io/react-antd-admin-template-doc/" target="_blank">react-antd-admin-template开发文档</a>。
+    目前正在编写完善中...
+  `
   return (
     <div className="app-container">
       <TypingCard title='开发文档' source={cardContent}/>
