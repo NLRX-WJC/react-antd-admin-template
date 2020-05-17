4206b351c7af21e10d8f892985348f261bc145e0
NLRX-WJC <414961362@qq.com>
2020-05-17 22:49:43 +0800

    refactor: 将logo图片改为svg引入

--- src/views/layout/Sider/Logo/index.jsx
+++ src/views/layout/Sider/Logo/index.jsx
@@ -1,14 +1,10 @@
 import React from "react";
-import logo from "@/assets/images/logo.png"
+import logo from "@/assets/images/logo.svg";
 import "./index.less";
 const Logo = () => {
   return (
     <div className="sidebar-logo-container">
-      <img
-        src={logo}
-        className="sidebar-logo"
-        alt=""
-      />
+      <img src={logo} className="sidebar-logo" alt="logo" />
       <h1 className="sidebar-title">难凉热血</h1>
     </div>
   );
