c7eb193e60d25271f73a9547274c9e889e8a1ca6
NLRX-WJC <414961362@qq.com>
2020-05-22 13:06:55 +0800

    refactor: 使用hooks重构404组件

--- src/views/error/404/index.jsx
+++ src/views/error/404/index.jsx
@@ -1,35 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
 import { Button, Row, Col } from "antd";
-import { connect } from "react-redux";
-import errImg from '@/assets/images/404.png'
+import errImg from "@/assets/images/404.png";
 import "./index.less";
 
-/*
-前台404页面
- */
-class NotFound extends Component {
-  goHome = () => {
-    this.props.history.replace("/");
-  };
+const NotFound = (props) => {
+  const { history } = props;
+  const goHome = () => history.replace("/");
+  return (
+    <Row className="not-found">
+      <Col span={12}>
+        <img src={errImg} alt="404" />
+      </Col>
+      <Col span={12} className="right">
+        <h1>404</h1>
+        <h2>抱歉，你访问的页面不存在</h2>
+        <div>
+          <Button type="primary" onClick={goHome}>
+            回到首页
+          </Button>
+        </div>
+      </Col>
+    </Row>
+  );
+};
 
-  render() {
-    return (
-      <Row className="not-found">
-        <Col span={12}>
-          <img src={errImg} alt="404"/>
-        </Col>
-        <Col span={12} className="right">
-          <h1>404</h1>
-          <h2>抱歉，你访问的页面不存在</h2>
-          <div>
-            <Button type="primary" onClick={this.goHome}>
-              回到首页
-            </Button>
-          </div>
-        </Col>
-      </Row>
-    );
-  }
-}
-
-export default connect()(NotFound);
+export default NotFound;
