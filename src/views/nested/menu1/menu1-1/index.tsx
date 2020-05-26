4b300b553e87561e937106ec9ea3f697ce944e3c
NLRX-WJC <414961362@qq.com>
2020-05-26 12:21:42 +0800

    refactor: 使用ts重构nested页面

---
+++ src/views/nested/menu1/menu1-1/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const Menu1_1: React.SFC = () => {
+  return <h1>Menu1-1</h1>;
+};
+
+export default Menu1_1;
---
+++ src/views/nested/menu1/menu1-2/menu1-2-1/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const Menu1_2_1: React.SFC = () => {
+  return <h1>Menu1_2_1-1</h1>;
+};
+
+export default Menu1_2_1;
