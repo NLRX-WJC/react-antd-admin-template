6c2f7a681dc4c20d4aef6658f9aa275bc5211e7c
NLRX-WJC <414961362@qq.com>
2020-06-02 12:47:56 +0800

    chore: 添加sentry埋点配置

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { Component } from 'react';
+import * as Sentry from '@sentry/browser';
 import { Provider } from "react-redux";
 import { ConfigProvider } from "antd";
 import zhCN from "antd/es/locale/zh_CN";
 import store from "./store";
 import Router from "./router";
 
-const App = () => {
-  return (
-    <ConfigProvider locale={zhCN}>
-      <Provider store={store}>
-        <Router />
-      </Provider>
-    </ConfigProvider>
-  );
-};
-
+class App extends Component {
+  // Sentry 埋点配置
+  componentDidCatch(error, errorInfo) {
+    Sentry.withScope((scope) => {
+      Object.keys(errorInfo).forEach((key) => {
+        scope.setExtra(key, errorInfo[key]);
+      });
+      Sentry.captureException(error);
+    });
+  }
+  render() { 
+    return (
+      <ConfigProvider locale={zhCN}>
+        <Provider store={store}>
+          <Router />
+        </Provider>
+      </ConfigProvider>
+    );
+  }
+}
+ 
 export default App;
